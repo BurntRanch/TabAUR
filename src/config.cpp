27445de1f531a56cf17bb3d5a9328f32d3bd2ce7
Toni500git <toni500minecraft@gmail.com>
2024-03-26 16:57:51 +0100

    fixing colors

--- src/config.cpp
+++ src/config.cpp
@@ -81,24 +81,24 @@ void Config::loadConfigFile(string filename) {
 void Config::loadColors() {
     if (this->colors) {
         NOCOLOR = "\033[0m";
-        BOLD    = "\033[0;1m";
-        BLACK   = "\033[0;30m";
-        RED     = "\033[0;31m";
-        GREEN   = "\033[0;32m";
-        YELLOW  = "\033[0;33m";
-        BLUE    = "\033[0;34m";
-        MAGENTA = "\033[0;35m";
-        CYAN    = "\033[0;36m";
-        WHITE   = "\033[0;37m";
+        BOLD    = "\033[0m\033[1m";
+        BLACK   = "\033[0m\033[30m";
+        RED     = "\033[0m\033[31m";
+        GREEN   = "\033[0m\033[32m";
+        YELLOW  = "\033[0m\033[33m";
+        BLUE    = "\033[0m\033[34m";
+        MAGENTA = "\033[0m\033[35m";
+        CYAN    = "\033[0m\033[36m";
+        WHITE   = "\033[0m\033[37m";
 
-        BOLDBLACK   = "\033[1;30m";
-        BOLDRED     = "\033[1;31m";
-        BOLDGREEN   = "\033[1;32m";
-        BOLDYELLOW  = "\033[1;33m";
-        BOLDBLUE    = "\033[1;34m";
-        BOLDMAGENTA = "\033[1;35m";
-        BOLDCYAN    = "\033[1;36m";
-        BOLDWHITE   = "\033[1;37m";
+        BOLDBLACK   = "\033[1m\033[30m";
+        BOLDRED     = "\033[1m\033[31m";
+        BOLDGREEN   = "\033[1m\033[32m";
+        BOLDYELLOW  = "\033[1m\033[33m";
+        BOLDBLUE    = "\033[1m\033[34m";
+        BOLDMAGENTA = "\033[1m\033[35m";
+        BOLDCYAN    = "\033[1m\033[36m";
+        BOLDWHITE   = "\033[1m\033[37m";
     } else {
         NOCOLOR = "";
         BOLD    = "";
