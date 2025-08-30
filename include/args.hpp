fe4ac88f9a6d28040668602e8c92dab526ba0a88
Toni500git <toni500minecraft@gmail.com>
2025-08-30 13:26:13 +0200

    update pacman license comments
    let's hope it's done right

--- include/args.hpp
+++ include/args.hpp
@@ -1,20 +1,9 @@
 /*
+ * Portions of this code are derived from Pacman
+ * Copyright (c) 2006-2025 Pacman Development Team <pacman-dev@lists.archlinux.org>
+ * Copyright (c) 2002-2006 by Judd Vinet <jvinet@zeroflux.org>
  *
- *  Copyright (c) 2006-2022 Pacman Development Team <pacman-dev@lists.archlinux.org>
- *  Copyright (c) 2002-2006 by Judd Vinet <jvinet@zeroflux.org>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Licensed under the GNU General Public License, version 2 or later.
  */
 
 #ifndef ARGS_HPP
